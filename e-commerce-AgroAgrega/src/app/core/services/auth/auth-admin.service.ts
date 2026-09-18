9e43d0dd1adf77eb81a3b40bef0e2164cddb8f6a
Igor Dias Oliveira <dev.diasx@gmail.com>
2026-09-18 09:50:53 -0300

    teste do bacalhau raivoso

--- e-commerce-AgroAgrega/src/app/core/services/auth/auth-admin.service.ts
+++ e-commerce-AgroAgrega/src/app/core/services/auth/auth-admin.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
+import UserDb from '@mocks/users.json';
 
 @Injectable({ providedIn: 'root' })
 export class AuthAdminService {
   public isLoggedIn: boolean = false;
 
   LoginAdmin(usuario: string, senha: string): boolean {
-    // Mock - troque depois por chamada real a uma API
-    if (usuario === 'admin' && senha === 'admin123') {
-      this.isLoggedIn = true;
-      return true;
-    }
+    return Object.values(UserDb).every(user => {
+      this.isLoggedIn = user.name === usuario && user.password === senha
+      ? true : false;
 
-    this.isLoggedIn = false;
-    return false;
+      return this.isLoggedIn;
+    });
   }
 }
